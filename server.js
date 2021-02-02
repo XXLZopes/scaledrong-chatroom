0c54b0aaf56c0c421aa2009380b589a8d0f1c9fc
XXLZopes <afordm99@gmail.com>
2021-02-02 12:31:14 -0800

    everything

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './index.html'));
+})
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT http://localhost:${PORT}`);
+});
